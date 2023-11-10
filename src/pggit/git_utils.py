8b9f833ed37125f6bcf598778a84f013ce029690
Alex <stevez@playgo.ai>
2023-11-11 00:50:22 +0800

    wip

--- src/pggit/git_utils.py
+++ src/pggit/git_utils.py
@@ -5,21 +5,23 @@ providing some tools to manage repository
 import os
 import subprocess
 
-def is_repo(path:str):
+def is_repo_root(path:str):
     '''check if the path is a root directory of a repository'''
+
     try:
         if not os.path.exists(path):
-            print("Error occurred in is_repo: No such directory")
+            print("Error occurred in is_repo_root: No such directory")
             return False
-        root=subprocess.check_output(["git","-C",path,"rev-parse","--show-toplevel"]).decode('utf-8').strip('\n')
-        if os.path.samefile(root,path):
+        git_command = ["git", "-C", path, "rev-parse", "--show-toplevel"]
+        root = subprocess.check_output(git_command).decode('utf-8').strip('\n')
+        if os.path.samefile(root, path):
             return True
     except Exception as e:
-        print(f"Error {type(e)} occurred in is_repo: {e}")
+        print(f"Error {type(e)} occurred in is_repo_root: {e}")
     return False
 
-def git_reset(rootdir:str,commitid:str="HEAD",hard:bool=False):
-    '''execute command 'git reset --hard commitid', rollback to the version in commitid hardly
+def git_reset(rootdir:str):
+    '''execute command 'git reset --hard HEAD^', rollback to the version in HEAD^ hardly
     
     params:
         rootdir: the root directory of a repository
@@ -27,48 +29,46 @@ def git_reset(rootdir:str,commitid:str="HEAD",hard:bool=False):
         hard: whether delete changes staged and in cwd
     '''
     if not os.path.exists(rootdir):
-        print("Error occurred in git_reset_to_HEAD: rootdir not exists")
+        print("Error occurred in git_reset: rootdir not exists")
         return False
-    if not is_repo(rootdir):
-        print(f"Error occurred in git_reset_to_HEAD: there is not a repository in {rootdir}")
+    if not is_repo_root(rootdir):
+        print(f"Error occurred in git_reset: there is not a repository in {rootdir}")
         return False
     
-    cwd=os.getcwd()
+    cwd = os.getcwd()
     try:
         os.chdir(rootdir)
         # stage all before reset
-        subprocess.call(["git","add","."])
-        command=["git","reset"]
-        if hard:
-            command.append("--hard")
-        command.append(commitid)
+        command = ["git", "add", "."]
+        subprocess.call(command)
+        # reset
+        command = ["git","reset", "--hard", "HEAD^"]
         subprocess.call(command)
         os.chdir(cwd)
         return True
     except Exception as e:
-        print(f"Error {type(e)} occurred in git_reset_to_HEAD: {e}")
+        print(f"Error {type(e)} occurred in git_reset: {e}")
         os.chdir(cwd)
         return False
 
-
     
-def git_diff_content(rootdir:str,beforeId:str,afterId:str):
-    '''get files content changed between two committing, 
-    changes are from beforeId to afterId
+def git_diff_content(rootdir:str, beforeId:str, afterId:str):
+    '''get file content changed between two commits,
+    changes are between beforeId and afterId
 
     params:
         rootdir: the root directory of a repository
         beforeId: the commit id earlier committed
         afterId: the commit id later commidtted
     return:
-        if succeed, return a dict with filename as key and list of changed lines as value\n
+        if succeed, return a dict with filename as key and list of changed lines as value
             return a empty dict if nothing changed
         if failed, return None
     '''
     if not os.path.exists(rootdir):
         print("Error occurred in git_diff_content: rootdir not exists")
         return None
-    if not is_repo(rootdir):
+    if not is_repo_root(rootdir):
         print(f"Error occurred in git_diff_content: there is not a repository in {rootdir}")
         return None
         
@@ -99,7 +99,7 @@ def git_current_commitid(rootdir:str):
     if not os.path.exists(rootdir):
         print("Error occurred in git_current_commitid: rootdir not exists")
         return None
-    if not is_repo(rootdir):
+    if not is_repo_root(rootdir):
         print(f"Error occurred in git_current_commitid: there is not a repository in {rootdir}")
         return None
     cwd=os.getcwd()
@@ -118,7 +118,7 @@ def git_config(rootdir:str,key:str,value:str,domain="--global"):
     if not os.path.exists(rootdir):
         print("Error occurred in git_current_commitid: rootdir not exists")
         return False
-    if not is_repo(rootdir):
+    if not is_repo_root(rootdir):
         print(f"Error occurred in git_current_commitid: there is not a repository in {rootdir}")
         return False
     cwd=os.getcwd()
@@ -140,7 +140,7 @@ def git_config(rootdir:str,key:str,value:str,domain="--global"):
 
 
 if __name__=="__main__":
-    print(is_repo("123"))
+    print(is_repo_root("123"))
     # print("testing git_reset_to_HEAD")
     # rootdir='test'
     # commitid="0a72079cb7f308e86a3b5c47086965140503c1e1"
