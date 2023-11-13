aa111f0a3ece7fe7079b1b4e7698b3cbdef2d194
Beracles <zhoujiang45@qq.com>
2023-11-13 22:26:03 +0800

    optimize diff_content

--- src/pggit/git_utils.py
+++ src/pggit/git_utils.py
@@ -83,8 +83,9 @@ def git_config(rootdir: str, key: str, value: str) -> bool:
         return False
 
 
-def git_diff_nameonly(rootdir: str, beforeId: str, afterId: str):
-    """get changed file list between two commits, from beforeId to afterId
+def git_diff_files(rootdir: str, beforeId: str, afterId: str):
+    """get changed files' name and their status between two commits,
+        from beforeId to afterId
 
     Args:
         rootdir (str): the root directory of the repository
@@ -92,23 +93,29 @@ def git_diff_nameonly(rootdir: str, beforeId: str, afterId: str):
         afterId (str): the commit id later committed
 
     Returns:
-        list | None: if succeed return a list including changed files' name
+        dict[str, str] | None: if succeed return a dict with
+            changed files' name as key and their status as value
     """
     if not os.path.exists(rootdir):
-        print("[git_diff] error: rootdir not exists")
+        print("[git_diff_files] error: rootdir not exists")
         return None
     if not is_repo_root(rootdir):
-        print(f"[git_diff] error: {rootdir} is not a repo")
+        print(f"[git_diff_files] error: {rootdir} is not a repo")
         return None
 
     try:
-        command = ["git", "diff", beforeId, afterId, "--name-only"]
+        command = ["git", "diff", beforeId, afterId, "--name-status"]
         res = subprocess.check_output(command, cwd=rootdir).decode("utf-8")
         res = res.replace("/", os.sep).splitlines()
-        return res
     except Exception as e:
-        print(f"[git_diff] error: {e}")
+        print(f"[git_diff_files] error: {e}")
         return None
+    outp = {}
+    for line in res:
+        status = line[0]
+        filename = line[1:].strip().strip('"')
+        outp[filename] = status
+    return outp
 
 
 def git_diff_content(rootdir: str, filepath: str, beforeId: str, afterId: str):
@@ -123,9 +130,6 @@ def git_diff_content(rootdir: str, filepath: str, beforeId: str, afterId: str):
 
     ```
     {
-        "newfile": "filename",    # name of the new file
-        "delfile": "filename",    # name of the deleted file
-        "rename": "filename",     # the new name of the file
         "delline": {lineindex1:"linestring1",
                     ...
                     lineindexn:"linestringn"}, # deleted lines
@@ -152,48 +156,27 @@ def git_diff_content(rootdir: str, filepath: str, beforeId: str, afterId: str):
         print(f"[git_diff_content] error: {e}")
         return None
 
-    outp = dict[str, Union[str, dict[int, str]]]()
-    a = ""
-    b = ""
-    old = {"start": 1, "len": 0, "cur": 1}
-    new = {"start": 1, "len": 0, "cur": 1}
+    outp = dict[str, dict[int, str]]()
+    oldcur = 1
+    newcur = 1
     for line in res:
-        if line.startswith("diff"):
-            tmp = re.match('.*a/(.*) .*b/(.*)"?', line).groups()
-            a = tmp[0].strip('"')
-            b = tmp[1].strip('"')
-            if a != b:
-                outp["rename"] = b
-                break
-        if line.startswith("---"):
-            tmpa = line.split()[1]
-            if tmpa == "/dev/null":
-                outp["newfile"] = b
-                break
-        if line.startswith("+++"):
-            tmpb = line.split()[1]
-            if tmpb == "/dev/null":
-                outp["delfile"] = a
-                break
         if line.startswith("@@"):
             tmp = line.split()
-            tmpa = map(int, tmp[1].strip("-").split(","))
-            tmpb = map(int, tmp[2].strip("+").split(","))
-            old["start"], old["len"], old["cur"] = tmpa[0], tmpa[1], tmpa[0]
-            new["start"], new["len"], new["cur"] = tmpb[0], tmpb[1], tmpb[0]
+            oldcur = int(tmp[1].strip("-").split(",")[0])
+            newcur = int(tmp[2].strip("-").split(",")[0])
             continue
         if line.startswith("-"):
             if "delline" not in outp:
                 outp["delline"] = {}
-            outp["delline"][old["cur"]] = line[1:]
-            old["cur"] += 1
+            outp["delline"][oldcur] = line[1:]
+            oldcur += 1
             continue
         if line.startswith("+"):
             if "addline" not in outp:
                 outp["addline"] = {}
-            outp["addline"][new["cur"]] = line[1:]
-            new["cur"] += 1
+            outp["addline"][newcur] = line[1:]
+            newcur += 1
             continue
-        old["cur"] += 1
-        new["cur"] += 1
+        oldcur += 1
+        newcur += 1
     return outp
