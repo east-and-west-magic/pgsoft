4c8c876af7253357bb50c154222b0bfccac7f350
Steve <stevez@playgo.ai>
2023-11-24 20:46:23 -0800

    add const

---
+++ pgsoft/pgconst/const.py
@@ -0,0 +1,21 @@
+#######################
+
+service_list = [
+    "games",
+    "greet",
+    "tokenization",
+    "upload game",
+    "download game",
+]
+
+game_list = [
+    "angry birds",
+    "snake",
+]
+
+functionality_list = [
+    "AI",
+    "functionality",
+    "json dedup",
+    "json",
+]
