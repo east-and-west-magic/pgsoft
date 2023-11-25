4e6fd438a499f0bb1dcc6e3850ef42ca575db278
Steve <stevez@playgo.ai>
2023-11-24 18:31:34 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 requirements = []
 setup(
     name="pgsoft",
-    version="v0.2-beta.2",
-    author="Beracles",
+    version="v1.2.0", # do not modify
+    author="PGSoft",
     license="MIT",
     url="git@github.com:east-and-west-magic/pgsoft.git",
     packages=find_packages(),
     requires=requirements,
-    description="""a package of some general tools for python developing""",
+    description="""General Python tools""",
 )
