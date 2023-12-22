881e02f4ad18250b6a14c0201cd230c3159158df
Steve <stevez@playgo.ai>
2023-12-22 15:38:44 +0800

    init ver

---
+++ pgsoft/pgconst/const.py
@@ -0,0 +1,30 @@
+"""
+Constants that are used in several repos.
+
+"""
+
+####################################
+# from pgsoft.pgconst import const
+#
+# print(const.service_list)
+####################################
+
+service_list = [
+    "games",
+    "greet",
+    "tokenization",
+    "upload game",
+    "download game",
+]
+
+game_list = [
+    "angry birds",
+    "snake",
+]
+
+functionality_list = [
+    "AI",
+    "functionality",
+    "json dedup",
+    "json",
+]
